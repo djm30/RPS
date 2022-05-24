518b18e54ba32bdcaa5ddc871782c7864bcc7b06
40265748 <dmorrison18@qub.ac.uk>
2022-05-24 14:30:15 +0100

    UI plus Javascript completed

--- script.js
+++ script.js
@@ -2,32 +2,70 @@
 let comScore = 0;
 let pScore = 0;
 
-choices = ["rock", "paper", "scissors"];
+let imgPath = "./images";
+let choices = ["rock", "paper", "scissors"];
+let gameOver = false;
+
+// REQUIRED DOM ELEMENTS
+const inputs = document.querySelectorAll(".button");
+const pImg = document.querySelector(".player .portrait img");
+const cImg = document.querySelector(".computer .portrait img");
+const pScoreContainer = document.querySelector(".pScore");
+const comScoreContainer = document.querySelector(".comScore");
+const results = document.querySelector(".results");
+
+const finalResult = document.querySelector(".final-result");
+const finalScore = document.querySelector(".final-score");
+const playAgainPopUp = document.querySelector(".play-again");
+const playAgainBtn = document.querySelector(".replay");
+
+// UPDATE SCORE
+const updateScore = () => {
+    pScoreContainer.innerText = pScore;
+    comScoreContainer.innerText = comScore;
+};
+
+// UPDATE IMG
+const updateImg = (choice, img) => {
+    img.src = `${imgPath}/${choices[choice]}.svg`;
+};
 
 const choiceToInteger = (choice) => choices.indexOf(choice);
 
 // FUNCTION TO GET COMPUTER CHOICE
-const getAIChoice = () => choices[Math.floor(Math.random() * 3)];
+const getAIChoice = () => Math.floor(Math.random() * 3);
 
-// FUNCTION TO GET PLAYER CHOICE
-const getPChoice = () => {
-    console.log("Please enter either Rock, Paper or Scissors");
-    let choice = "";
-    let validChoice = false;
-    while (!validChoice) {
-        choice = prompt("Enter choice:").toLowerCase();
-        if (choice === "paper" || choice === "rock" || choice === "scissors") {
-            validChoice = true;
-        }
+const checkGameOver = () => {
+    if (pScore >= 5 || comScore >= 5) {
+        gameOver = true;
     }
-    return choice;
+};
+
+const whenGameOver = () => {
+    let winner = pScore > comScore ? "You Win!" : "You Loose!";
+    finalResult.innerText = `${winner}\nFinal Score:`;
+    finalScore.innerText = `${pScore} - ${comScore}`;
+    setTimeout(() => {
+        playAgainPopUp.classList.remove("hidden");
+    }, 1000);
+};
+
+const reset = () => {
+    pScore = 0;
+    comScore = 0;
+    updateScore();
+    pImg.src = "";
+    cImg.src = "";
+    results.innerText = "";
+    playAgainPopUp.classList.add("hidden");
+    gameOver = false;
 };
 
 // GET WINNER
 const getWinner = (pChoice, AIChoice) => {
     let playerWin = false;
     let tieGame = false;
-    let comparison = choiceToInteger(pChoice) - choiceToInteger(AIChoice);
+    let comparison = pChoice - AIChoice;
     switch (comparison) {
         case 0:
             tieGame = true;
@@ -38,50 +76,49 @@ const getWinner = (pChoice, AIChoice) => {
     }
 
     let messageString = "";
+
+    results.classList.remove("tie", "win", "loose");
+
     if (tieGame) {
+        results.classList.add("tie");
         messageString = "Tie Game!";
     } else {
         if (playerWin) {
+            results.classList.add("win");
             messageString = "You win!";
             pScore++;
         } else {
+            results.classList.add("loose");
             messageString = "Computer Wins!";
             comScore++;
         }
     }
-
-    console.log(
-        `You chose ${pChoice}, Computer chose: ${AIChoice}\n${messageString}`
-    );
+    return `You chose ${choices[pChoice]}, Computer chose: ${choices[AIChoice]}\n${messageString}`;
 };
 
-// SCORE PRINT
-const printScore = () => {
-    console.log(`Computer Score: ${comScore}\tPlayer Score: ${pScore}`);
-};
+const playRound = (pChoice) => {
+    // GET AI CHOICE
+    const AIChoice = getAIChoice();
 
-const finalScore = () => {
-    let winner = "";
-    if (pScore === comScore) {
-        winner = "Tie Game!";
-    } else if (pScore > comScore) {
-        winner = "Player Wins!";
-    } else {
-        winner = "Computer Wins!";
+    // UPDATING IMGS TO REFLECT CHOICES
+    updateImg(pChoice, pImg);
+    updateImg(AIChoice, cImg);
+    const message = getWinner(pChoice, AIChoice);
+    results.innerText = message;
+    updateScore();
+    checkGameOver();
+    if (gameOver) {
+        whenGameOver();
     }
-
-    return `Final Scores:\nComputer: ${comScore}\t Player: ${pScore}\n${winner}`;
 };
 
-const playRound = () => {
-    getWinner(getPChoice(), getAIChoice());
-    printScore();
-};
+// ATTACH CLICK LISTENERS TO INPUTS
+inputs.forEach((input) => {
+    input.addEventListener("click", (e) => {
+        // MAKE PLAYER CHOICE WITH CLICKED BUTTONS DATA ATTRIBUTE
+        if (gameOver) return;
+        playRound(input.dataset.value);
+    });
+});
 
-for (let _ = 0; _ < 5; _++) {
-    console.log(`Round ${_ + 1}`);
-    playRound();
-}
-console.log(finalScore());
-
-// 5 ROUND GAME
+playAgainBtn.addEventListener("click", reset);
